66007543f1dafa29f064b8c94cf122ddb38d9711
ventil <ventillo@gmail.com>
2015-08-06 06:57:01 +0200

    Admin - dsaboard left side menu removed and consolidated to top

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -116,7 +116,7 @@ tinymce.init({
           </ul>
         </div> -->
         <!-- This is where the fun begins -->
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <div class="main"> <!-- col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2  was in the class -->
         <? include './includes/'.$section.'.php'; ?>
         </div>
       </div>
